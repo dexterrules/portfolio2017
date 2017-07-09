c0f89d398de959591e5628343adfee83725d7acd
Sanchit Soni <chitquizzer@gmail.com>
2017-07-09 10:36:12 -0700

    updated

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ app.get('/', (request, response) => {
   response.sendFile(path.join(__dirname, '')+'/index.html');
 })
 
-app.listen(port, (err) => {  
-  if (err) {
-    return console.log('something bad happened', err)
-  }
-
-  console.log(`server is listening on ${port}`)
-})
+// app.listen(port, (err) => {
+//   if (err) {
+//     return console.log('something bad happened', err)
+//   }
+//
+//   console.log(`server is listening on ${port}`)
+// })
 
